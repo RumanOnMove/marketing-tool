e9bd4f56cf19d030e354e4c3e9ba3be946066c70
RumanOnMove <oruman@moveon.com.bd>
2023-07-18 17:31:26 +0600

    declare email template controller, email template service provider, api route

---
+++ packages/Moveon/EmailTemplate/src/Http/Controllers/EmailTemplateController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Moveon\EmailTemplate\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class EmailTemplateController extends Controller
+{
+
+}
---
+++ packages/Moveon/EmailTemplate/src/Providers/EmailTemplateServiceProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Moveon\EmailTemplate\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class EmailTemplateServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/api.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+    }
+}
